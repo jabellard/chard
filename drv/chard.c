55721127d59dc538d9df34d82b6d9b4744098ad1
Joe Nathan Abellard <joenatech@gmail.com>
2017-09-28 02:43:50 -0400

    successfully implemented cdev-based solution

--- drv/chard.c
+++ drv/chard.c
@@ -3,70 +3,72 @@
 #include <linux/device.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
+#include <linux/types.h>
 #include <asm/uaccess.h>
 #include "chard.h"
+#include <linux/cdev.h>
 
 MODULE_DESCRIPTION("chard: character device.");
 MODULE_AUTHOR("Joe Nathan Abellard");
 MODULE_LICENSE("GPL");
-MODULE_VERSION("1.0");
+MODULE_VERSION("1.1");
 
 
 
 
 static struct file_operations fops =
 {
+	.owner = THIS_MODULE,
 	.open = chard_open,
 	.read = chard_read,
 	.write = chard_write,
 	.release = chard_release
 }; // end struct fops
 
-static int major_number;
-static char memory_area[MEMORY_AREA_SIZE];
-static int data_size;
+int major_number;
+int first_minor_number = 0;
 
-static struct class *chard_class = NULL;
-static struct device *chard_device = NULL;
+chard d0;
+int data_size;
 
 
 
 static int __init chard_init(void)
 {
-	//printk(KERN_INFO "chard:.\n");
-	printk(KERN_INFO "chard:init....\n");
-
-	// dynamically obtain a major number
-	major_number = register_chrdev(0, DEVICE_NAME, &fops);
-	printk(KERN_INFO "chard:init1....\n");
-
-	// register the class
-	chard_class = class_create(THIS_MODULE, CLASS_NAME);
-	printk(KERN_INFO "chard:init2....\n");
-
-	// register the device driver
-	chard_device = device_create(chard_class, NULL, MKDEV(major_number, 0), NULL, DEVICE_NAME);
-	printk(KERN_INFO "chard:init3....\n");
-
+	int result;
+	dev_t dev = 0;
+	
+	result = alloc_chrdev_region(&dev, first_minor_number, 1, "chard");
+	major_number = MAJOR(dev);
+	
+	// init the device
+	chard_setup_cdev(&d0);
 	return 0;
 } // end chard_init()
 
+static void chard_setup_cdev(chard *dev)
+{
+	int err;
+	int devno = MKDEV(major_number, first_minor_number);
+	
+	cdev_init(&dev->device, &fops);
+	dev->device.owner = THIS_MODULE;
+	dev->device.ops = &fops;
+	err = cdev_add(&dev->device, devno, 1);
+} // end chard_setup_cdev()
+
+
 static void __exit chard_exit(void)
 {
-	// remove the device
-	device_destroy(chard_class, MKDEV(major_number, 0));
+
+	dev_t devno = MKDEV(major_number, 0);
+	cdev_del(&d0.device);
 	
-	// unregister device class
-	class_unregister(chard_class);
-	
-	// remove device class
-	class_destroy(chard_class);
-	
-	// unregister dev #
-	unregister_chrdev(major_number, DEVICE_NAME);
-	printk(KERN_INFO "chard:DONE....\n");
-	
+	unregister_chrdev_region(devno, 1);
 } // end chard_exit()
+
+
+
 
 static int chard_open(struct inode *inodep, struct file *filep)
 {
@@ -79,7 +81,7 @@ static ssize_t chard_read(struct file *filep, char *buffer, size_t len, loff_t *
 {
 	printk(KERN_INFO "READING.\n");
 	int ret = 0;
-	ret = copy_to_user(buffer, memory_area, data_size); 
+	ret = copy_to_user(buffer, &d0.memory_area, data_size); 
 	
 	return (data_size = 0);
 	
@@ -88,8 +90,8 @@ static ssize_t chard_read(struct file *filep, char *buffer, size_t len, loff_t *
 
 static ssize_t chard_write(struct file *filep, const char *buffer, size_t len, loff_t *offset)
 {
- 	sprintf(memory_area, "%s(%zu letters)", buffer, len); 
- 	data_size = strlen(memory_area);
+ 	sprintf(&d0.memory_area, "%s(%zu letters)", buffer, len); 
+ 	data_size = strlen(&d0.memory_area);
  	return len;
 } // end chard_write()
 
--- drv/chard.h
+++ drv/chard.h
@@ -2,14 +2,25 @@
 #define CHARD_H
 
 #define MEMORY_AREA_SIZE 256
+#define NUM_DEVICES 1
 #define DEVICE_NAME "chard0"
 #define CLASS_NAME "chard"
+#include <linux/cdev.h>
+
+
+typedef struct _chard
+{
+	char memory_area[MEMORY_AREA_SIZE];
+	struct cdev device;
+	struct semaphore sem;
+}chard; // end struct _chard
+
 
 // function prototypes------------------
 static int chard_open(struct inode *inodep, struct file *filep);
 static ssize_t chard_read(struct file *filep, char *buffer, size_t len, loff_t *offset);
 static ssize_t chard_write(struct file *filep, const char *buffer, size_t len, loff_t *offset);
 static int chard_release(struct inode *inodep, struct file *filep);
-
+static void chard_setup_cdev(chard *dev);
 
 #endif // CHARD_H
