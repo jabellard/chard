bf682b020ef34d2bb9602aa2c55e334e722a1125
Joe Nathan Abellard <joenatech@gmail.com>
2017-10-03 01:33:50 -0400

    updated loading script

--- lm.sh
+++ lm.sh
@@ -1,29 +1,16 @@
 #!/bin/sh
-# $Id: scull_load,v 1.4 2004/11/03 06:19:49 rubini Exp $
+
 module="chard"
 device="chard"
-mode="777"
+mode="755"
 
-# Group: since distributions do it differently, look for wheel or use staff
-if grep -q '^staff:' /etc/group; then
-    group="staff"
-else
-    group="wheel"
-fi
-
-# invoke insmod with all arguments we got
-# and use a pathname, as insmod doesn't look in . by default
+# load the module
 /sbin/insmod ./drv/$module.ko $* || exit 1
 
-# retrieve major number
+# retrieve the module's major number
 major=$(awk "\$2==\"$module\" {print \$1}" /proc/devices)
 
-# Remove stale nodes and replace them, then give gid and perms
-# Usually the script is shorter, it's scull that has several devices in it.
-
+# remove possilbe stale node, and create a new device node
 rm -f /dev/${device}0
 mknod /dev/${device}0 c $major 0
-ln -sf /dev/${device}0 ./${device}0
-#chgrp $group /dev/${device}0
-#chmod $mode  /dev/${device}0
 
